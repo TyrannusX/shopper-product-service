79d31aedb0df806eb7f798de485c02a48a26b563
Robert Reyes <neorob22@gmail.com>
2024-03-24 09:29:38 -0500

    added env file

--- infrastructure/brokers.py
+++ infrastructure/brokers.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
 import pika
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+RABBIT_MQ_HOSTNAME = os.getenv("RABBIT_MQ_HOSTNAME")
+RABBIT_MQ_USERNAME = os.getenv("RABBIT_MQ_USERNAME")
+RABBIT_MQ_PASSWORD = os.getenv("RABBIT_MQ_PASSWORD")
 
 
 class MessageBroker(ABC):
@@ -11,8 +18,8 @@ class MessageBroker(ABC):
 
 class RabbitMqBroker(MessageBroker):
     async def publish(self, message, event):
-        credentials = pika.PlainCredentials('guest', 'guest')
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq", port=5672, credentials=credentials))
+        credentials = pika.PlainCredentials(RABBIT_MQ_USERNAME, RABBIT_MQ_PASSWORD)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_MQ_HOSTNAME, port=5672, credentials=credentials))
         channel = connection.channel()
         channel.exchange_declare(exchange=event, exchange_type="fanout", durable=True)
         channel.queue_declare(queue=event, durable=True)
--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+MONGODB_HOST_NAME = os.getenv("MONGODB_HOST_NAME")
 
-client = MongoClient("mongodb", 27017)
+client = MongoClient(MONGODB_HOST_NAME, 27017)
 db = client.product
 collection: Collection = db.products
